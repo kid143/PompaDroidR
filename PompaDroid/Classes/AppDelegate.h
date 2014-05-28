ecf596c9e66f2017cf9844d83201f18e2b776cbb
sciencetech <primecriminalkid@gmail.com>
2014-05-28 14:38:02 +0800

    Initial Commit

---
+++ PompaDroid/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  PompaDroid
+//
+//  Created by kid143 on 14-5-28.
+//  Copyright kid143 2014年. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ PompaDroid/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PompaDroid
+//
+//  Created by kid143 on 14-5-28.
+//  Copyright kid143 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
